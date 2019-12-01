778695b2d81af555598a68d3cd9a1d5afaa80190
firzhan <firzhan007@gmail.com>
2019-12-02 02:58:19 +0530

    removed unnecessary folders

--- email-services/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS email_store;
-
-CREATE TABLE email_store (
-            id LONG AUTO_INCREMENT,
-            email_content VARCHAR(4000) NOT NULL,
-            email_status INT NOT NULL DEFAULT 0,
-            PRIMARY KEY (id)
-);
